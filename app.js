04180f76a6598896c4d3eff16e8d8cb1569339b8
RadkaHrabcova <radkahrabcova@gmail.com>
2024-09-05 13:20:57 +0200

    display dice number

--- app.js
+++ app.js
@@ -8,9 +8,24 @@ points = [0,0];
 pointsInTheRound = 0;
 activePlayer = 0;
 finalPoints = 25;
-dice = Math.floor(Math.random()*6)+1;
-console.log(dice);
 
-document.querySelector("#soucasne-" + activePlayer).textContent = dice;
+
+
+document.getElementById("body-0").textContent = "0";
+document.getElementById("body-1").textContent = "0";
+document.getElementById("soucasne-0").textContent = "0";
+document.getElementById("soucasne-1").textContent = "0";
+
+//
 
 document.querySelector(".kostka").style.display = "none";
+
+document.querySelector(".tlacitko-hod").addEventListener("click", function(){
+    //1.Nahodne cislo
+    dice = Math.floor(Math.random()*6)+1;
+    //2. Zobrazit vysledek
+    let diceDOM = document.querySelector(".kostka");
+    diceDOM.style.display = "block";
+    diceDOM.textContent = dice;
+    //3. Aktualizovat body kola pokud padla/nepadla 1
+});
