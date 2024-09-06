e9af8baafab44a9482019c27fe0e244dcd33dc7a
RadkaHrabcova <radkahrabcova@gmail.com>
2024-09-06 12:41:19 +0200

    set winner function

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ let activePlayer;
 let dice;
 let finalPoints;
 
-points = [0,0];
+points = [0, 0];
 pointsInTheRound = 0;
 activePlayer = 0;
-finalPoints = 25;
+finalPoints = 15;
 
 
 
@@ -20,29 +20,53 @@ document.getElementById("soucasne-1").textContent = "0";
 
 document.querySelector(".kostka").style.display = "none";
 
-document.querySelector(".tlacitko-hod").addEventListener("click", function(){
+document.querySelector(".tlacitko-hod").addEventListener("click", function () {
     //1.Random number
-    dice = Math.floor(Math.random()*6)+1;
+    dice = Math.floor(Math.random() * 6) + 1;
     //2. Display result
     let diceDOM = document.querySelector(".kostka");
     diceDOM.style.display = "block";
     diceDOM.textContent = dice;
     //3. Update the points in the round if 1 is rolled on the dice
-    if (dice !== 1){
+    if (dice !== 1) {
         //Add points
         pointsInTheRound += dice;
         document.querySelector("#soucasne-" + activePlayer).textContent = pointsInTheRound;
     } else {
-        //Switch players
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        pointsInTheRound = 0;
-
-        document.getElementById("soucasne-0").textContent = "0";
-        document.getElementById("soucasne-1").textContent = "0";    
-
-        document.querySelector(".hrac-0-panel").classList.toggle("aktivni");
-        document.querySelector(".hrac-1-panel").classList.toggle("aktivni");
-
-        
+        //4.Switch players
+        switchPlayers()
     }
 });
+
+document.querySelector(".tlacitko-dost").addEventListener("click", function () {
+    //1.Add points in the round to the player points
+    points[activePlayer] += pointsInTheRound;
+    //2.Update UI
+    document.querySelector("#body-" + activePlayer).textContent = points[activePlayer];
+    //3. Check if the player wins.
+    if(points[activePlayer]>= finalPoints){
+        document.querySelector("#jmeno-" + activePlayer).textContent = "Vítěz!"
+        document.querySelector(".hrac-" + activePlayer + "-panel").classList.remove("aktivni");
+        document.querySelector(".hrac-" + activePlayer + "-panel").classList.add("vitez");
+        document.querySelector(".kostka").style.display = "none";
+    }else{
+        switchPlayers()
+    }
+
+    //4. Switch players.
+  
+
+
+});
+
+function switchPlayers(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    pointsInTheRound = 0;
+
+    document.getElementById("soucasne-0").textContent = "0";
+    document.getElementById("soucasne-1").textContent = "0";
+
+    document.querySelector(".hrac-0-panel").classList.toggle("aktivni");
+    document.querySelector(".hrac-1-panel").classList.toggle("aktivni");
+
+};
